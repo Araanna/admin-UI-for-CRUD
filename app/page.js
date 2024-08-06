d14b625faad5cba0dd85eb234d5cb86d2b3e1ab3
CodeByAdaMela <melanieabalde0@gmail.com>
2024-08-06 22:14:24 +0800

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,205 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table, Button, Form, Modal, Col, Row } from "react-bootstrap";
+import EditProducts from "./components/EditProducts"; // Adjust the path as necessary
+import AddProduct from "./components/AddProduct"; // Adjust path as necessary
+import DeleteProduct from "./components/deleteProduct"; // Adjust path as necessary
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const AdminDashboard = ({ user }) => {
+  const [products, setProducts] = useState([]);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [productToDelete, setProductToDelete] = useState(null);
+  const [currentProduct, setCurrentProduct] = useState({
+    barcode: "",
+    name: "",
+    price: 0,
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const response = await axios.get("http://localhost/pos/products.php");
+  //       const data = response.data;
+  //       setProducts(
+  //         Object.entries(data.products).map(([barcode, { name, price }]) => ({
+  //           barcode,
+  //           name,
+  //           price,
+  //         }))
+  //       );
+  //     } catch (error) {
+  //       setError(error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   fetchProducts();
+  // }, []);
+
+  // const handleAddProduct = async () => {
+  //   try {
+  //     const response = await axios.post(
+  //       "http://localhost/pos/add_product.php", // URL of your PHP script
+  //       currentProduct,
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json", // Ensure content type is JSON
+  //         },
+  //       }
+  //     );
+  //     console.log("Server response:", response.data); // Debugging line
+  //     if (response.data.success) {
+  //       setProducts([...products, currentProduct]); // Add new product to state
+  //       setShowAddModal(false);
+  //       setCurrentProduct({ barcode: "", name: "", price: 0 }); // Reset form
+  //     } else {
+  //       setError("Failed to add product: " + response.data.message);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error adding product:", error); // Debugging line
+  //     setError("An error occurred while adding the product.");
+  //   }
+  // };
+
+  // const handleEditProduct = async () => {
+  //   try {
+  //     console.log("Sending product update:", currentProduct); // Debugging line
+  //     const response = await axios.put(
+  //       `http://localhost/pos/update_product.php`,
+  //       currentProduct
+  //     );
+  //     console.log("Server response:", response.data); // Debugging line
+  //     if (response.data.success) {
+  //       const updatedProducts = products.map((product) =>
+  //         product.barcode === currentProduct.barcode ? currentProduct : product
+  //       );
+  //       setProducts(updatedProducts);
+  //       setShowEditModal(false);
+  //     } else {
+  //       setError("Failed to update product.");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error updating product:", error); // Debugging line
+  //     setError("An error occurred while updating the product.");
+  //   }
+  // };
+
+  // const handleDeleteProduct = async (barcode) => {
+  //   try {
+  //     await axios.delete(
+  //       `http://localhost/pos/delete_product.php?barcode=${barcode}`
+  //     );
+  //     setProducts(products.filter((product) => product.barcode !== barcode));
+  //   } catch (error) {
+  //     setError(error);
+  //   }
+  // };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCurrentProduct((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="container mx-auto p-5">
+        <Col>
+          <div className="p-10 bg-purple-500 text-white rounded shadow-lg">
+            <h2 className="text-3xl font-semibold border-b-8 w-[17vw]">
+              Admin Dashboard
+            </h2>
+          </div>
+        </Col>
+        <div className="my-4">
+          <Button variant="dark" onClick={() => setShowAddModal(true)}>
+            Add Product
+          </Button>
         </div>
+        <Row>
+          <div className="mx-4 my-4 w-full">
+            <Table striped bordered hover className="mt-3">
+              <thead className="bg-purple-300 sticky top-0 z-10">
+                <tr>
+                  <th>Barcode</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <ScrollArea>
+                <tbody>
+                  {products.map((product) => (
+                    <tr key={product.barcode}>
+                      <td>{product.barcode}</td>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>
+                        <Button
+                          variant="warning"
+                          onClick={() => {
+                            setCurrentProduct(product);
+                            setShowEditModal(true);
+                          }}
+                        >
+                          Edit
+                        </Button>{" "}
+                        <Button
+                          variant="danger"
+                          onClick={() => {
+                            setProductToDelete(product);
+                            setShowDeleteModal(true);
+                          }}
+                        >
+                          Delete
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </ScrollArea>
+            </Table>
+          </div>
+        </Row>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Include the AddProduct component */}
+      <AddProduct
+        showAddModal={showAddModal}
+        setShowAddModal={setShowAddModal}
+        currentProduct={currentProduct}
+        handleChange={handleChange}
+        // handleAddProduct={handleAddProduct}
+      />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Include the EditProducts component */}
+      <EditProducts
+        showEditModal={showEditModal}
+        setShowEditModal={setShowEditModal}
+        currentProduct={currentProduct}
+        handleChange={handleChange}
+        // handleEditProduct={handleEditProduct}
+      />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {/* Include the DeleteProduct component */}
+      <DeleteProduct
+        showDeleteModal={showDeleteModal}
+        setShowDeleteModal={setShowDeleteModal}
+        // currentProduct={productToDelete}
+        // handleDeleteProduct={handleDeleteProduct}
+      />
+    </>
   );
-}
+};
+
+export default AdminDashboard;
